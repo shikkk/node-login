271040e2ed5ddf96c4b397abd11d11ecdd7c7c55
shizhikai <shizhikai@cmcm.com>
2019-08-01 16:14:39 +0800

    修改登录后跳转

--- app.js
+++ app.js
@@ -68,13 +68,7 @@ app.post('/dologin', function(req, res, next) {
         findcollection.find(params).toArray(function(error,docs){
             if(docs.length>0){
                 session.userinfo = docs[0]
-                // res.redirect('/product')
-                res.status(200),
-                    res.json([{
-                        name:'shihzikai'
-                    },{
-                        name:'admin'
-                    }])
+                res.redirect('/product')
             }else{
                 res.send('<script>alert("没有该用户，请注册！");location.href="/login"</script>');
             }
@@ -111,46 +105,3 @@ app.use(function(req, res, next) {
 
 
 app.listen(3000)
-
-/*
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var bodyParser = require('body-parser')
-var app = require('./routes/all');
-
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-//
-// var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
-
-*/
